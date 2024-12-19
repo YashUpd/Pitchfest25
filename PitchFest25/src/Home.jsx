89b43ea7f2c085746d1513dd7fe7d746cd81f39a
HUmnanANirudh <tanirudhganesh@gmail.com>
2024-12-19 19:19:35 +0530

    update

--- PitchFest25/src/Home.jsx
+++ PitchFest25/src/Home.jsx
@@ -4,10 +4,13 @@ import Navbar from "./Navbar";
 
 const Home = ()=>{
     return(
+       
         <div className="bg-custom-gradient w-full min-h-screen flex flex-col justify-center items-center">
+            <section id="home">
             <Banner/>
             <Navbar/>
             <Hero/>
+            </section>
         </div>
     )
 }
--- PitchFest25/src/Navbar.jsx
+++ PitchFest25/src/Navbar.jsx
@@ -2,10 +2,10 @@ const Navbar = () =>{
     return(
         <div>
             <ul className="text-white font-semibold flex justify-center items-center gap-10 pt-6 text-xl">
-                <li className="px-2">HOME</li>
-                <li className="px-2">BENEFITS</li>
-                <li className="px-2">REGISTER</li>
-                <li className="px-2">PARTNERS</li>
+                <li className="px-2"><a href="#home" className="nav-link hover:text-gray-400 focus:text-yellow-500 focus:outline-none">HOME</a></li>
+                <li className="px-2"><a href="#benefits" className="nav-link hover:text-gray-400 focus:text-yellow-500 focus:outline-none">BENEFITS</a></li>
+                <li className="px-2"><a href="#registration" className="nav-link hover:text-gray-400 focus:text-yellow-500 focus:outline-none">REGISTER</a></li>
+                <li className="px-2"><a href="#partners" className="nav-link hover:text-gray-400 focus:text-yellow-500 focus:outline-none">PARTNERS</a></li>
             </ul>
         </div>
     )

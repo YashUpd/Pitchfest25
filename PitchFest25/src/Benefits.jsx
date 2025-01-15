7b63eb8a4b1e457a55c4b87f2dd21a009f91acbb
YashUpd <yash20upd@gmail.com>
2025-01-16 00:03:37 +0530

    commented out the benefits text

--- PitchFest25/src/Benefits.jsx
+++ PitchFest25/src/Benefits.jsx
@@ -4,7 +4,7 @@ const Benefits = () => {
   return (
     <div className="bg-custom-gradient flex flex-col items-center mb-20">
       <h1 className="text-center text-yellow-text text-4xl font-semibold uppercase m-10">
-        Benefits
+        {/* Benefits */}
       </h1>
       <Categories/>
     </div>

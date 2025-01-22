075557f9e6d58a6c835c4fc0b69b77dd6f0e7c6b
HUmnanANirudh <tanirudhganesh@gmail.com>
2025-01-23 00:47:51 +0530

    Update

--- PitchFest25/src/Register.jsx
+++ PitchFest25/src/Register.jsx
@@ -8,50 +8,50 @@ const Register = () => {
     );
   };
   return (
-    <div className="flex justify-center items-center bg-white rounded-xl shadow-2xl mt-6 mb-6 md:mt-6 w-[85%] md:w-[80%]">
-      <div className="flex flex-col justify-center  items-center p-2 md:p-4 gap-2 md:gap-4">
-        <div className="w-full flex justify-center">
+    <div className="flex justify-center items-center bg-white rounded md:rounded-xl shadow-2xl mt-6 mb-6 md:mt-6 w-[90%] md:w-[80%]">
+      <div className="flex flex-col justify-center items-center p-2 md:p-4 gap-2 md:gap-4">
+        <div className="w-full flex justify-center ">
           <img
             src="/assets/Head.png"
-            className="w-full rounded-xl object-cover"
+            className="w-full rounded-md object-cover"
           />
         </div>
-        <div className="flex flex-col md:flex-row justify-center md:items-stretch items-center w-full flex-grow">
-          <div className="md:w-1/3 h-auto object-cover">
+        <div className="flex justify-center items-center w-full">
+          <div className="w-[40%] md:w-[45%] h-full object-cover p-2">
             <img
               src="/assets/Register.png"
-              className="w-full h-40 md:w-full object-cover md:h-full sm:object-contain md:object-scale-down"
+              className="w-full md:w-full object-cover md:h-full sm:object-contain md:object-scale-down"
             />
           </div>
-          <div className="p-1 md:p-2 bg-[#FFD42E8A] md:w-2/3 rounded-xl">
-            <div className="flex flex-col mb-2 md:mb-4 px-2">
-            <h1 className="text-lg sm:text-xl md:text-2xl xl:text-3xl font-bold text-black md:py-2 py-0.5 ">
+          <div className="flex flex-col justify-around gap-2 py-2 md:p-2 bg-yellow-background w-2/3 rounded-lg">
+            <div className="flex flex-col gap-2 px-2 mb-1 md:mb-2 py-2">
+            <h1 className="text-[10px] sm:text-lg md:text-2xl font-bold text-black md:py-2 mb-1">
               Ready to Take Your Startup to the Next Level?
             </h1>
-            <p className="text-black text-justify text-xs sm:text-[12px] xl:text-xl mb-1 md:mb-2">
+            <p className="text-black text-justify text-[6px] sm:text-xs md:text-lg md:mb-2 mb-1">
               Don't miss out on this incredible opportunity to showcase your
               innovation, connect with industry leaders, and secure funding for
               your startup. PitchFest 2025 is your chance to shine in front of
               top investors and mentors.
             </p>
             </div>
-            <div className="flex flex-col mb-1 p-2">
-            <h1 className="text-lg sm:text-xl md:text-2xl xl:text-3xl font-bold text-black py-2">
+            <div className="flex flex-col gap-2 md:mb-1 px-2">
+            <h1 className="text-[10px] sm:text-lg md:text-2xl font-bold text-black md:py-2 mb-1">
               Register Now and Get Ready to Pitch!
             </h1>
-            <p className="text-black text-justify text-xs sm:text-[12px] xl:text-xl mb-2">
+            <p className="text-black text-justify text-[6px] sm:text-xs md:text-lg">
               Take the first step towards transforming your idea into a thriving
               business. Click the button below to start your registration
               process.
             </p>
-            </div>
+            <div className="my-1 md:my-4">
             <button
-              className="bg-white font-normal py-1 md:px-3 px-2 text-[#FCC15A] text-base sm:text-lg flex justify-start items-center shadow-xl rounded-lg mx-1 md:mx-2 my-1"
+              className="bg-white font-normal py-0 px-0.5 md:px-2 md:py-1 text-[#FCC15A] text-[10px] sm:text-xs md:text-base flex justify-start items-center shadow-xl rounded-sm"
               onClick={redirect}
             >
               Register Here
               <motion.span
-              className="pt-1"
+              className="md:pt-0.5"
                 animate={{ x: [0, 6, 0] }}
                 transition={{
                   duration: 2,
@@ -63,10 +63,12 @@ const Register = () => {
                 <ChevronsRight />
               </motion.span>
             </button>
+            </div>
+            </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 export default Register;

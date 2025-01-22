49ed9308a295586e2f6797356f52f9abd7d1a53f
HUmnanANirudh <tanirudhganesh@gmail.com>
2025-01-22 19:01:12 +0530

    Update

--- PitchFest25/src/Categories.jsx
+++ PitchFest25/src/Categories.jsx
@@ -1,81 +1,91 @@
-import Images from "./Images"
 import Textbox from "./Textbox"
-
 const Categories = () => {
   return (
-    <div className="bg-white rounded-2xl w-[90%] md:w-[80%] mx-auto">
-      <div className="flex flex-col p-4">
-        <h1 className="text-center text-2xl md:text-3xl font-bold text-blue-text mb-6">
-          Two Tracks
-        </h1>
-        <div className="flex flex-col md:flex-row justify-evenly gap-4 md:gap-10 items-center">
-          <img
-            src='/assets/top1.png'
-            className="hidden md:block bg-custom-gradient p-4 rounded-full"
-          />
-          <Textbox text={"Revenue"} />
-          <Textbox text={"Pre-Revenue"} />
-          <img
-            src='/assets/top2.png'
-            className="hidden md:block bg-custom-gradient p-4 rounded-full"
-          />
-        </div>
-      </div>
-      <div className="flex flex-col items-center gap-5 p-4 mb-10 md:mb-20">
-        <h1 className="text-center text-2xl md:text-3xl font-bold text-blue-text pb-2">
-          Focus Areas
-        </h1>
-        <div className="grid grid-cols-2 md:flex md:justify-between gap-6 md:gap-12">
-          <div className="flex flex-col items-center">
-            <Images img={'/assets/women_funded.png'} />
-            <h1 className="text-center text-sm md:text-base">
-              Women
-              <br />
-              Founded
-            </h1>
-          </div>
-          <div className="flex flex-col items-center">
-            <Images img={'/assets/smart.png'} />
-            <h1 className="text-center text-sm md:text-base">
-              Smart
-              <br />
-              Manufacturing
-            </h1>
-          </div>
-          <div className="flex flex-col items-center">
-            <Images img={'/assets/Sustainable.png'} />
-            <h1 className="text-center text-sm md:text-base">
-              Sustainability
-              <br />& Social Impact
-            </h1>
-          </div>
-          <div className="flex flex-col items-center">
-            <Images img={'/assets/Medical.png'} />
-            <h1 className="text-center text-sm md:text-base">
-              Medical
-              <br />
-              Devices
-            </h1>
+    <div className="flex justify-center w-[85%] md:w-[80%] max-w-7xl mx-auto">
+      <div className="bg-white rounded-2xl w-full">
+        <div className="flex flex-col md:p-4 p-2">
+          <h1 className="text-center text-2xl md:text-3xl font-bold text-blue-text mb-6">
+            Two Tracks
+          </h1>
+          <div className="flex justify-center md:gap-10 gap-2 items-center">
+            <div className="bg-custom-gradient flex justify-center w-full md:w-80 p-2 md:p-4 rounded-full">
+              <img
+                src='/assets/top1.png'
+                className=" w-full max-w-[3rem] sm:max-w-[4rem] md:max-w-[5rem] xl:max-w-[6rem]object-contain"
+              />
+            </div>
+            <Textbox text={"Revenue"} />
+            <Textbox text={"Pre-Revenue"} />
+            <div className="bg-custom-gradient flex justify-center w-full md:w-80 p-2 md:p-4 rounded-full">
+              <img
+                src='/assets/top2.png'
+                className="w-full max-w-[3rem] sm:max-w-[4rem] md:max-w-[5rem] xl:max-w-[6rem] object-contain"
+              />
+            </div>
           </div>
         </div>
-        <div className="grid grid-cols-2 md:flex md:justify-between gap-6 md:gap-12 mt-2 md:mt-0">
-          <div className="flex flex-col items-center">
-            <Images img={'/assets/clean.png'} />
-            <h1 className="whitespace-normal md:whitespace-nowrap text-sm md:text-base text-center">Clean Energy</h1>
+        <div className="flex flex-col items-center gap-6 p-4 mb-10 md:mb-20">
+          <h1 className="text-center text-2xl md:text-3xl font-bold text-blue-text pb-2">
+            Focus Areas
+          </h1>
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-6 md:gap-12">
+            <div className="flex flex-col items-center">
+              <div className="bg-custom-gradient p-6 rounded-full w-full flex justify-center">
+                <img src='/assets/women_funded.png' className="w-full max-w-[3rem] md:max-w-[5rem] object-contain" />
+              </div>
+              <h1 className="text-center text-sm md:text-base font-medium">
+                Women <br /> Founded
+              </h1>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="bg-custom-gradient p-6 rounded-full w-full flex justify-center">
+                <img src='/assets/smart.png' className="w-full max-w-[3rem] md:max-w-[5rem] object-contain" />
+              </div>
+              <h1 className="text-center text-sm md:text-base font-medium">
+                Smart <br /> Manufacturing
+              </h1>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="bg-custom-gradient p-6 rounded-full w-full flex justify-center">
+                <img src='/assets/Sustainable.png' className="w-full max-w-[3rem] md:max-w-[5rem] object-contain" />
+              </div>
+              <h1 className="text-center text-sm md:text-base font-medium">
+                Sustainability <br />& Social Impact
+              </h1>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="bg-custom-gradient p-6 rounded-full w-full flex justify-center">
+                <img src='/assets/Medical.png' className="w-full max-w-[3rem] md:max-w-[5rem] object-contain" />
+              </div>
+              <h1 className="text-center text-sm md:text-base font-medium">
+                Medical
+              </h1>
+            </div>
           </div>
-          <div className="flex flex-col items-center">
-            <Images img={'/assets/deep.png'} />
-            <h1 className="whitespace-normal md:whitespace-nowrap text-sm md:text-base text-center">Deep Tech</h1>
-          </div>
-          <div className="flex flex-col items-center col-span-2 justify-self-center md:col-span-1">
-            <Images img={'/assets/Mobility.png'} />
-            <h1 className="whitespace-normal md:whitespace-nowrap text-sm md:text-base text-center">Mobility</h1>
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-10 md:gap-12 mt-4 sm:mt-4 xl:mt-4">
+            <div className="flex flex-col items-center">
+              <div className="bg-custom-gradient  rounded-full w-full p-6 md:p-6">
+                <img src='/assets/clean.png' className="w-full max-w-[3rem] md:max-w-[5rem] object-contain sm:p-2 md:p-1" />
+              </div>
+              <h1 className="text-center text-sm md:text-base font-medium">Clean Energy</h1>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="bg-custom-gradient rounded-full w-full flex justify-center p-6 md:p-6">
+                <img src='/assets/deep.png' className="w-full max-w-[3rem] md:max-w-[5rem] object-contain sm:p-2 md:p-1 " />
+              </div>
+              <h1 className="text-center text-sm md:text-base font-medium">Deep Tech</h1>
+            </div>
+            <div className="flex flex-col items-center self-center mx-auto col-span-2 md:col-span-1">
+              <div className="bg-custom-gradient p-6 md:p-6 rounded-full w-full">
+                <img src='/assets/Mobility.png' className="w-full max-w-[3rem] md:max-w-[5rem] object-contain sm:p-2 md:p-1" />
+              </div>
+              <h1 className="text-center text-sm md:text-base font-medium">Mobility</h1>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Categories
-
+export default Categories;

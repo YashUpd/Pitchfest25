6048ca0ff535f8056c1062cf7f53adb5ff5db9b5
YashUpd <yash20upd@gmail.com>
2025-02-02 15:58:17 +0530

    changed the aboutus content

--- PitchFest25/src/Aboutus.jsx
+++ PitchFest25/src/Aboutus.jsx
@@ -15,15 +15,7 @@ const Aboutus = () => {
         <div className="w-full md:w-2/3 bg-[#FFD42E8A] rounded-xl flex flex-col justify-around items-start md:px-4 md:py-2 px-3">
           <h1 className="text-lg sm:text-xl md:text-2xl xl:text-3xl font-bold text-black py-2">About Us</h1>
           <p className="text-black text-justify text-xs sm:text-[12px] xl:text-xl ">
-            PitchFest 2025, hosted by BML Munjal University, is the ultimate
-            launchpad for innovative startups to make their mark! Powered by
-            ACIC-BMU | Propel, this thrilling event brings together bold
-            entrepreneurs, visionary investors, and industry game-changers. It’s
-            not just about pitching — it’s about unlocking massive
-            opportunities, securing funding, and gaining invaluable mentorship.
-            With a spotlight on fresh, disruptive ideas, PitchFest 2025 is your
-            chance to connect, collaborate, and propel your startup to the next
-            level in one of the most dynamic startup ecosystems around!
+          Propel Pitchfest25, hosted by ACIC BMU - the incubator at BML Munjal University, is the ultimate launchpad for innovative startups to make their mark! This thrilling event brings together bold entrepreneurs, visionary investors, and industry game-changers. It’s not just about pitching — it’s about unlocking massive opportunities, securing funding, and gaining invaluable mentorship. With a spotlight on fresh, disruptive ideas, revenue and pre-revenue startup ideas come to pitch, making this a golden opportunity for founders at all stages. Propel Pitchfest25 is your chance to connect, collaborate, and propel your startup to the next level in one of the most dynamic startup ecosystems around!
           </p>
           <div className="flex justify-start items-center my-2 md:my-4 md:pt-3 py-2">
             <button className="bg-white font-normal py-1 px-4 text-[#FCC15A] text-base md:text-lg flex justify-start items-center shadow-xl rounded-md"
